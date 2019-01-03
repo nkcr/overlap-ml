8024b0807217bf342d694af720472ba2b0d4b9e7
Noémien Kocher <nkcr.je@gmail.com>
2019-01-03 13:38:16 +0100

    Improves logging, saving, formatting and device management

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
-import os, sys
+import os
+import sys
 import time
 import math
 import numpy as np
@@ -13,13 +14,17 @@ import gc
 import data
 import model
 
-from utils import batchify, get_batch, repackage_hidden, create_exp_dir, save_checkpoint
+from utils import batchify, get_batch, repackage_hidden, create_exp_dir, \
+    save_checkpoint, prepare_dir, get_logger, set_utils_logger, init_device, \
+    save_args, save_commit_id, TensorBoard
 
-parser = argparse.ArgumentParser(description='PyTorch PennTreeBank/WikiText2 RNN/LSTM Language Model')
+parser = argparse.ArgumentParser(
+    description='PyTorch PennTreeBank/WikiText2 RNN/LSTM Language Model')
 parser.add_argument('--data', type=str, default='./penn/',
                     help='location of the data corpus')
 parser.add_argument('--model', type=str, default='LSTM',
-                    help='type of recurrent net (RNN_TANH, RNN_RELU, LSTM, GRU, SRU)')
+                    help='type of recurrent net '
+                    '(RNN_TANH, RNN_RELU, LSTM, GRU, SRU)')
 parser.add_argument('--emsize', type=int, default=400,
                     help='size of word embeddings')
 parser.add_argument('--nhid', type=int, default=1150,
@@ -45,27 +50,39 @@ parser.add_argument('--dropouth', type=float, default=0.3,
 parser.add_argument('--dropouti', type=float, default=0.65,
                     help='dropout for input embedding layers (0 = no dropout)')
 parser.add_argument('--dropoute', type=float, default=0.1,
-                    help='dropout to remove words from embedding layer (0 = no dropout)')
+                    help='dropout to remove words from embedding layer '
+                    '(0 = no dropout)')
 parser.add_argument('--dropoutl', type=float, default=-0.2,
                     help='dropout applied to layers (0 = no dropout)')
 parser.add_argument('--wdrop', type=float, default=0.5,
-                    help='amount of weight dropout to apply to the RNN hidden to hidden matrix')
+                    help='amount of weight dropout to apply to the RNN hidden '
+                    'to hidden matrix')
 parser.add_argument('--tied', action='store_false',
                     help='tie the word embedding and softmax weights')
 parser.add_argument('--seed', type=int, default=1111,
                     help='random seed')
 parser.add_argument('--nonmono', type=int, default=5,
                     help='random seed')
-parser.add_argument('--cuda', action='store_false',
-                    help='use CUDA')
+parser.add_argument('--no-cuda', action='store_true',
+                    help='do NOT use CUDA')
 parser.add_argument('--log-interval', type=int, default=200, metavar='N',
                     help='report interval')
-parser.add_argument('--save', type=str,  default='EXP',
-                    help='path to save the final model')
+# parser.add_argument('--save', type=str,  default='EXP',
+#                     help='path to save the final model')
+parser.add_argument('--log-dir', type=str, default='logs',
+                    help="Directory containing the runs")
+parser.add_argument('--model-dir', type=str, help='Directory of the run. '
+                    'If not specified, one is created based on the time.')
+parser.add_argument('--model-dir-prefix', type=str,
+                    help='A prefix to be added if the model-dir is '
+                    'automatically created. Has no effect if --model-dir '
+                    'is specified.')
 parser.add_argument('--alpha', type=float, default=2,
-                    help='alpha L2 regularization on RNN activation (alpha = 0 means no regularization)')
+                    help='alpha L2 regularization on RNN activation '
+                    '(alpha = 0 means no regularization)')
 parser.add_argument('--beta', type=float, default=1,
-                    help='beta slowness regularization applied on RNN activiation (beta = 0 means no regularization)')
+                    help='beta slowness regularization applied on RNN '
+                    'activiation (beta = 0 means no regularization)')
 parser.add_argument('--wdecay', type=float, default=1.2e-6,
                     help='weight decay applied to all weights')
 parser.add_argument('--continue_train', action='store_true',
@@ -73,12 +90,14 @@ parser.add_argument('--continue_train', action='store_true',
 parser.add_argument('--n_experts', type=int, default=10,
                     help='number of experts')
 parser.add_argument('--small_batch_size', type=int, default=-1,
-                    help='the batch size for computation. batch_size should be divisible by small_batch_size.\
-                     In our implementation, we compute gradients with small_batch_size multiple times, and accumulate the gradients\
-                     until batch_size is reached. An update step is then performed.')
+                    help='the batch size for computation. batch_size should '
+                    'be divisible by small_batch_size. In our implementation, '
+                    'we compute gradients with small_batch_size multiple, '
+                    'times and accumulate the gradients until batch_size is '
+                    'reached. An update step is then performed.')
 parser.add_argument('--max_seq_len_delta', type=int, default=40,
                     help='max sequence length')
-parser.add_argument('--single_gpu', default=False, action='store_true', 
+parser.add_argument('--single_gpu', default=False, action='store_true',
                     help='use single GPU')
 args = parser.parse_args()
 
@@ -89,25 +108,22 @@ if args.dropoutl < 0:
 if args.small_batch_size < 0:
     args.small_batch_size = args.batch_size
 
-if not args.continue_train:
-    args.save = '{}-{}'.format(args.save, time.strftime("%Y%m%d-%H%M%S"))
-    create_exp_dir(args.save, scripts_to_save=['main.py', 'model.py'])
-
-def logging(s, print_=True, log_=True):
-    if print_:
-        print(s)
-    if log_:
-        with open(os.path.join(args.save, 'log.txt'), 'a+') as f_log:
-            f_log.write(s + '\n')
-
+# Prepare and sets the `model_dir`
+prepare_dir(args)
+# Logger init and set for utils
+logger = get_logger(args)
+set_utils_logger(logger)
 # Set the random seed manually for reproducibility.
 np.random.seed(args.seed)
 torch.manual_seed(args.seed)
-if torch.cuda.is_available():
-    if not args.cuda:
-        print("WARNING: You have a CUDA device, so you should probably run with --cuda")
-    else:
-        torch.cuda.manual_seed_all(args.seed)
+# Sets the `args.device` and CUDA seed.
+init_device(args)
+# Save infos
+save_args(args)
+save_commit_id(args)
+# Tensorboard
+tb = TensorBoard(args.model_dir)
+
 
 ###############################################################################
 # Load data
@@ -127,29 +143,26 @@ test_data = batchify(corpus.test, test_batch_size, args)
 
 ntokens = len(corpus.dictionary)
 if args.continue_train:
-    model = torch.load(os.path.join(args.save, 'model.pt'))
+    model = torch.load(os.path.join(args.model_dir, 'model.pt'))
 else:
-    model = model.RNNModel(args.model, ntokens, args.emsize, args.nhid, args.nhidlast, args.nlayers, 
-                       args.dropout, args.dropouth, args.dropouti, args.dropoute, args.wdrop, 
-                       args.tied, args.dropoutl, args.n_experts)
+    model = model.RNNModel(args.model, ntokens, args.emsize, args.nhid,
+                           args.nhidlast, args.nlayers, args.dropout,
+                           args.dropouth, args.dropouti, args.dropoute,
+                           args.wdrop, args.tied, args.dropoutl,
+                           args.n_experts)
 
-if args.cuda:
-    if args.single_gpu:
-        parallel_model = model.cuda()
-    else:
-        parallel_model = nn.DataParallel(model, dim=1).cuda()
-else:
-    parallel_model = model
+parallel_model = model.to(args.device)
 
 total_params = sum(x.data.nelement() for x in model.parameters())
-logging('Args: {}'.format(args))
-logging('Model total parameters: {}'.format(total_params))
+logger.info('Args: {}'.format(args))
+logger.info('Model total parameters: {}'.format(total_params))
 
 criterion = nn.CrossEntropyLoss()
 
 ###############################################################################
 # Training code
 ###############################################################################
+
 
 def evaluate(data_source, batch_size=10):
     # Turn on evaluation mode which disables dropout.
@@ -163,7 +176,8 @@ def evaluate(data_source, batch_size=10):
             targets = targets.view(-1)
 
             log_prob, hidden = parallel_model(data, hidden)
-            loss = nn.functional.nll_loss(log_prob.view(-1, log_prob.size(2)), targets).data
+            loss = nn.functional.nll_loss(
+                log_prob.view(-1, log_prob.size(2)), targets).data
 
             total_loss += loss * len(data)
 
@@ -172,19 +186,22 @@ def evaluate(data_source, batch_size=10):
 
 
 def train():
-    assert args.batch_size % args.small_batch_size == 0, 'batch_size must be divisible by small_batch_size'
+    assert(args.batch_size % args.small_batch_size == 0,
+           'batch_size must be divisible by small_batch_size')
 
     # Turn on training mode which enables dropout.
     total_loss = 0
     start_time = time.time()
     ntokens = len(corpus.dictionary)
-    hidden = [model.init_hidden(args.small_batch_size) for _ in range(args.batch_size // args.small_batch_size)]
+    hidden = [model.init_hidden(args.small_batch_size) for _ in range(
+        args.batch_size // args.small_batch_size)]
     batch, i = 0, 0
     while i < train_data.size(0) - 1 - 1:
         bptt = args.bptt if np.random.random() < 0.95 else args.bptt / 2.
         # Prevent excessively small or negative sequence lengths
         seq_len = max(5, int(np.random.normal(bptt, 5)))
-        # There's a very small chance that it could select a very long sequence length resulting in OOM
+        # There's a very small chance that it could select a very long sequence
+        # length resulting in OOM
         seq_len = min(seq_len, args.bptt + args.max_seq_len_delta)
 
         lr2 = optimizer.param_groups[0]['lr']
@@ -196,20 +213,26 @@ def train():
 
         start, end, s_id = 0, args.small_batch_size, 0
         while start < args.batch_size:
-            cur_data, cur_targets = data[:, start: end], targets[:, start: end].contiguous().view(-1)
+            cur_data, cur_targets = data[:, start: end], targets[:, start: end].contiguous(
+            ).view(-1)
 
             # Starting each batch, we detach the hidden state from how it was previously produced.
             # If we didn't, the model would try backpropagating all the way to start of the dataset.
             hidden[s_id] = repackage_hidden(hidden[s_id])
 
-            log_prob, hidden[s_id], rnn_hs, dropped_rnn_hs = parallel_model(cur_data, hidden[s_id], return_h=True)
-            raw_loss = nn.functional.nll_loss(log_prob.view(-1, log_prob.size(2)), cur_targets)
+            log_prob, hidden[s_id], rnn_hs, dropped_rnn_hs = parallel_model(
+                cur_data, hidden[s_id], return_h=True)
+            raw_loss = nn.functional.nll_loss(
+                log_prob.view(-1, log_prob.size(2)), cur_targets)
 
             loss = raw_loss
             # Activiation Regularization
-            loss = loss + sum(args.alpha * dropped_rnn_h.pow(2).mean() for dropped_rnn_h in dropped_rnn_hs[-1:])
+            loss = loss + sum(args.alpha * dropped_rnn_h.pow(2).mean()
+                              for dropped_rnn_h in dropped_rnn_hs[-1:])
             # Temporal Activation Regularization (slowness)
-            loss = loss + sum(args.beta * (rnn_h[1:] - rnn_h[:-1]).pow(2).mean() for rnn_h in rnn_hs[-1:])
+            loss = loss + \
+                sum(args.beta * (rnn_h[1:] - rnn_h[:-1]
+                                 ).pow(2).mean() for rnn_h in rnn_hs[-1:])
             loss *= args.small_batch_size / args.batch_size
             total_loss += raw_loss.data * args.small_batch_size / args.batch_size
             loss.backward()
@@ -229,15 +252,17 @@ def train():
         if batch % args.log_interval == 0 and batch > 0:
             cur_loss = total_loss.item() / args.log_interval
             elapsed = time.time() - start_time
-            logging('| epoch {:3d} | {:5d}/{:5d} batches | lr {:02.2f} | ms/batch {:5.2f} | '
-                    'loss {:5.2f} | ppl {:8.2f}'.format(
-                epoch, batch, len(train_data) // args.bptt, optimizer.param_groups[0]['lr'],
-                elapsed * 1000 / args.log_interval, cur_loss, math.exp(cur_loss)))
+            logger.info('| epoch {:3d} | {:5d}/{:5d} batches | lr {:02.2f} | ms/batch {:5.2f} | '
+                        'loss {:5.2f} | ppl {:8.2f}'.format(
+                            epoch, batch, len(
+                                train_data) // args.bptt, optimizer.param_groups[0]['lr'],
+                            elapsed * 1000 / args.log_interval, cur_loss, math.exp(cur_loss)))
             total_loss = 0
             start_time = time.time()
         ###
         batch += 1
         i += seq_len
+
 
 # Loop over epochs.
 lr = args.lr
@@ -247,14 +272,18 @@ stored_loss = 100000000
 # At any point you can hit Ctrl + C to break out of training early.
 try:
     if args.continue_train:
-        optimizer_state = torch.load(os.path.join(args.save, 'optimizer.pt'))
+        optimizer_state = torch.load(
+            os.path.join(args.model_dir, 'optimizer.pt'))
         if 't0' in optimizer_state['param_groups'][0]:
-            optimizer = torch.optim.ASGD(model.parameters(), lr=args.lr, t0=0, lambd=0., weight_decay=args.wdecay)
+            optimizer = torch.optim.ASGD(
+                model.parameters(), lr=args.lr, t0=0, lambd=0., weight_decay=args.wdecay)
         else:
-            optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, weight_decay=args.wdecay)
+            optimizer = torch.optim.SGD(
+                model.parameters(), lr=args.lr, weight_decay=args.wdecay)
         optimizer.load_state_dict(optimizer_state)
     else:
-        optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, weight_decay=args.wdecay)
+        optimizer = torch.optim.SGD(
+            model.parameters(), lr=args.lr, weight_decay=args.wdecay)
 
     for epoch in range(1, args.epochs+1):
         epoch_start_time = time.time()
@@ -266,15 +295,15 @@ try:
                 prm.data = optimizer.state[prm]['ax'].clone()
 
             val_loss2 = evaluate(val_data)
-            logging('-' * 89)
-            logging('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | '
-                    'valid ppl {:8.2f}'.format(epoch, (time.time() - epoch_start_time),
-                                               val_loss2, math.exp(val_loss2)))
-            logging('-' * 89)
+            logger.info('-' * 89)
+            logger.info('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | '
+                        'valid ppl {:8.2f}'.format(epoch, (time.time() - epoch_start_time),
+                                                   val_loss2, math.exp(val_loss2)))
+            logger.info('-' * 89)
 
             if val_loss2 < stored_loss:
-                save_checkpoint(model, optimizer, args.save)
-                logging('Saving Averaged!')
+                save_checkpoint(model, optimizer, args)
+                logger.info('Saving Averaged!')
                 stored_loss = val_loss2
 
             for prm in model.parameters():
@@ -282,34 +311,35 @@ try:
 
         else:
             val_loss = evaluate(val_data, eval_batch_size)
-            logging('-' * 89)
-            logging('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | '
-                    'valid ppl {:8.2f}'.format(epoch, (time.time() - epoch_start_time),
-                                               val_loss, math.exp(val_loss)))
-            logging('-' * 89)
+            logger.info('-' * 89)
+            logger.info('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | '
+                        'valid ppl {:8.2f}'.format(epoch, (time.time() - epoch_start_time),
+                                                   val_loss, math.exp(val_loss)))
+            logger.info('-' * 89)
 
             if val_loss < stored_loss:
-                save_checkpoint(model, optimizer, args.save)
-                logging('Saving Normal!')
+                save_checkpoint(model, optimizer, args)
+                logger.info('Saving Normal!')
                 stored_loss = val_loss
 
-            if 't0' not in optimizer.param_groups[0] and (len(best_val_loss)>args.nonmono and val_loss > min(best_val_loss[:-args.nonmono])):
-                logging('Switching!')
-                optimizer = torch.optim.ASGD(model.parameters(), lr=args.lr, t0=0, lambd=0., weight_decay=args.wdecay)
+            if 't0' not in optimizer.param_groups[0] and (len(best_val_loss) > args.nonmono and val_loss > min(best_val_loss[:-args.nonmono])):
+                logger.info('Switching!')
+                optimizer = torch.optim.ASGD(
+                    model.parameters(), lr=args.lr, t0=0, lambd=0., weight_decay=args.wdecay)
                 #optimizer.param_groups[0]['lr'] /= 2.
             best_val_loss.append(val_loss)
 
 except KeyboardInterrupt:
-    logging('-' * 89)
-    logging('Exiting from training early')
+    logger.info('-' * 89)
+    logger.info('Exiting from training early')
 
 # Load the best saved model.
-model = torch.load(os.path.join(args.save, 'model.pt'))
-parallel_model = nn.DataParallel(model, dim=1).cuda()
+model = torch.load(os.path.join(args.model_dir, 'model.pt'))
+parallel_model = nn.DataParallel(model, dim=1).to(args.device)
 
 # Run on test data.
 test_loss = evaluate(test_data, test_batch_size)
-logging('=' * 89)
-logging('| End of training | test loss {:5.2f} | test ppl {:8.2f}'.format(
+logger.info('=' * 89)
+logger.info('| End of training | test loss {:5.2f} | test ppl {:8.2f}'.format(
     test_loss, math.exp(test_loss)))
-logging('=' * 89)
+logger.info('=' * 89)
--- utils.py
+++ utils.py
@@ -1,13 +1,28 @@
-import os, shutil
+import os
+import shutil
 import torch
 from torch.autograd import Variable
 
+from datetime import datetime
+from tensorboardX.summary import Summary
+import tensorboardX as tb
+
+import os
+import json
+import logging
+import subprocess
+
+logger = None
+
+
 def repackage_hidden(h):
-    """Wraps hidden states in new Variables, to detach them from their history."""
+    """Wraps hidden states in new Variables, to detach them from their history.
+    """
     if isinstance(h, tuple) or isinstance(h, list):
         return tuple(repackage_hidden(v) for v in h)
     else:
         return h.detach()
+
 
 def batchify(data, bsz, args):
     # Work out how cleanly we can divide the dataset into bsz parts.
@@ -15,11 +30,10 @@ def batchify(data, bsz, args):
     # Trim off any extra elements that wouldn't cleanly fit (remainders).
     data = data.narrow(0, 0, nbatch * bsz)
     # Evenly divide the data across the bsz batches.
-    data = data.view(bsz, -1).t().contiguous()
+    data = data.view(bsz, -1).t().contiguous().to(args.device)
     print(data.size())
-    if args.cuda:
-        data = data.cuda()
     return data
+
 
 def get_batch(source, i, args, seq_len=None):
     seq_len = min(seq_len if seq_len else args.bptt, len(source) - 1 - i)
@@ -27,6 +41,7 @@ def get_batch(source, i, args, seq_len=None):
     # target = Variable(source[i+1:i+1+seq_len].view(-1))
     target = Variable(source[i+1:i+1+seq_len])
     return data, target
+
 
 def create_exp_dir(path, scripts_to_save=None):
     if not os.path.exists(path):
@@ -39,10 +54,119 @@ def create_exp_dir(path, scripts_to_save=None):
             dst_file = os.path.join(path, 'scripts', os.path.basename(script))
             shutil.copyfile(script, dst_file)
 
-def save_checkpoint(model, optimizer, path, finetune=False):
-    if finetune:
-        torch.save(model, os.path.join(path, 'finetune_model.pt'))
-        torch.save(optimizer.state_dict(), os.path.join(path, 'finetune_optimizer.pt'))
+
+def save_checkpoint(model, optimizer, args, finetune=False):
+    model_name, opt_name = ('finetune_model.pt', 'finetune_optimizer.pt') \
+        if finetune else ('model.pt', 'optimizer.pt')
+    torch.save(model, os.path.join(args.model_dir, model_name))
+    torch.save(optimizer.state_dict(), os.path.join(args.model_dir, opt_name))
+
+
+def init_device(args):
+    """Sets the `args.device` attribute based on `args.no_cuda` and host
+    availability. It also sets the CUDA seed if needed.
+    """
+    if torch.cuda.is_available():
+        if args.no_cuda:
+            logger.info('WARNING: You have a CUDA device,'
+                        'so you should probably not run with --no-cuda')
+            setattr(args, 'device', 'cpu')
+        else:
+            if hasattr(args, 'seed'):
+                torch.cuda.manual_seed(args.seed)
+            setattr(args, 'device', args.cuda_device)
     else:
-        torch.save(model, os.path.join(path, 'model.pt'))
-        torch.save(optimizer.state_dict(), os.path.join(path, 'optimizer.pt'))
+        if not args.no_cuda:
+            logger.info('WARNING: No CUDA device found, using CPU. '
+                        'It would be best to explicitly run with --no-cuda')
+        setattr(args, 'device', 'cpu')
+    logger.info(f"Using device: {args.device}")
+
+
+def prepare_dir(args):
+    """Sets a model_dir if not specified in args and creates it."""
+    if args.model_dir is None:
+        model_dir = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        if args.model_dir_prefix is not None:
+            model_dir = f"{args.model_dir_prefix}-{model_dir}"
+        model_dir = os.path.join(args.log_dir, model_dir)
+        setattr(args, "model_dir", model_dir)
+    if not os.path.exists(model_dir):
+        print(f"Creating {model_dir}")
+        os.makedirs(model_dir)
+
+
+def save_commit_id(args, prefix=""):
+    """Saves the commit ID and the repo origin if using git."""
+    # Contains byte output
+    cmd = subprocess.run(["git", "rev-parse", "HEAD"],
+                         stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+    cmd2 = subprocess.run(["git", "config", "--get", "remote.origin.url"],
+                          stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+    if cmd.returncode == 0 and cmd.returncode == 0:
+        id = cmd.stdout
+        infos = cmd2.stdout
+    else:
+        logger.info("WARNING: Won't save GIT infos, commands return non-zero")
+        return
+
+    id_path = os.path.join(args.model_dir, prefix+"git.txt")
+    with open(id_path, 'wb') as fp:
+        fp.write(b"Commit ID used, GIT infos\n\n")
+        fp.write(id)
+        fp.write(infos)
+
+
+def save_args(args, prefix=""):
+    param_path = os.path.join(args.model_dir, prefix + "params.json")
+    with open(param_path, 'w') as fp:
+        json.dump(args.__dict__, fp, indent=4, sort_keys=True)
+
+
+def save_tb(tb, name, x, y):
+    """Saves a tensorboard sacalar as a (x,y) coordinate."""
+    tb.scalar_summary(name, y, x)
+
+
+def save_hist(tb, name, epoch, values):
+    """Saves a tensorboard histogram."""
+    tb.hist_summary(name, values, epoch)
+
+
+class TensorBoard(object):
+    def __init__(self, model_dir):
+        self.summary_writer = tb.FileWriter(model_dir)
+
+    def scalar_summary(self, tag, value, step):
+        summary = Summary(value=[Summary.Value(tag=tag, simple_value=value)])
+        self.summary_writer.add_summary(summary, global_step=step)
+
+    def hist_summary(self, tag, value, step):
+        hist_summary = tb.summary.histogram(tag, value, "auto")
+        self.summary_writer.add_summary(hist_summary, global_step=step)
+
+
+def get_logger(args, name=__name__, level=logging.INFO):
+    logger = logging.getLogger(name)
+    if getattr(logger, '_init_done__', None):
+        logger.setLevel(level)
+        return logger
+    logger._init_done__ = True
+    logger.propagate = False
+    logger.setLevel(level)
+    formatter = logging.Formatter("%(asctime)s:%(levelname)s::%(message)s")
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    handler.setLevel(0)
+    f_handler = logging.FileHandler(os.path.join(args.model_dir, 'file.log'))
+    f_handler.setFormatter(formatter)
+    f_handler.setLevel(0)
+    del logger.handlers[:]
+    logger.addHandler(handler)
+    logger.addHandler(f_handler)
+    return logger
+
+
+def set_utils_logger(my_logger):
+    global logger
+    logger = my_logger
